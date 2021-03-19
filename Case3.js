6c15a9e84664f5fe96462e4f02caf980a5f49db8
thegiesblack <reygienaldo@rocketmail.com>
2021-03-19 18:33:04 +0700

    The very first commit, and maybe no more.

---
+++ Case3.js
@@ -0,0 +1,146 @@
+const data = require('./Manifest/Case1.json');
+
+//init container for passenger and crew
+var passEven = [];
+var passOdd  = [];
+var crewEven = [];
+var crewOdd  = [];
+
+//split passenger and crew data based on age
+function convPass(data){
+    for (var i in data){
+        if (data[i].role == "passenger" && data[i].age%2 == 0){
+            passEven.push(data[i]);
+        }
+        else if (data[i].role == "passenger" && data[i].age%2 != 0){
+            passOdd.push(data[i]);
+        }
+    }
+}
+
+function convCrew(data){
+    for (var i in data){
+        if (data[i].role == "crew" && data[i].age%2 == 0){
+            crewEven.push(data[i]);
+        }
+        else if (data[i].role == "crew" && data[i].age%2 != 0){
+            crewOdd.push(data[i]);
+        }
+    }
+}
+
+convPass(data);
+convCrew(data);
+
+var boat     = 3;
+var lifeboat = [];
+for (var i = 0; i<boat;i++){
+    var newArr = new Array();
+    lifeboat.push(newArr);
+}
+
+var captain = {"name" : "Captain", "age" : 27};
+
+//determine which lifeboat the captain will be put
+captain.age % 2 == 0 ? lifeboat[1].push(captain.name) : lifeboat[0].push(captain.name);
+goToLifeboat(passEven, passOdd, crewEven, crewOdd);
+
+function goToLifeboat(passEven, passOdd, crewEven, crewOdd){
+    //concate all odd and even data
+    var allOdd = [];
+    allOdd     = allOdd.concat(passOdd);
+    allOdd     = allOdd.concat(crewOdd);
+
+    var allEven = [];
+    allEven     = allEven.concat(passEven);
+    allEven     = allEven.concat(crewEven);
+
+    //sort odd and even data based on age, and name alphabetically
+    allEven = allEven.sort(function(a, b) {
+      if (a.age > b.age) return 1;
+      if (a.age < b.age) return -1;
+
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
+    });
+
+    allOdd = allOdd.sort(function(a, b) {
+      if (a.age > b.age) return 1;
+      if (a.age < b.age) return -1;
+
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
+    });
+
+    for (var i=0;i<boat;i++){
+        var passengerMin        = 2;
+        var passengerCounter    = 0;
+        var personInBoat        = 0;
+        var arrIndexToBeRemoved = [];
+
+        var crewInBoat = 0;
+
+        var howManyPersonLeft   = lifeboat[i][0] == "Captain" ? 7 : 8;
+        var boatIsOddOrEven     = i%2==0 ? "odd" : "even";
+        var arrOfPerson         = boatIsOddOrEven=="even" ? allEven : allOdd;
+        for (var j=0; j<arrOfPerson.length; j++){
+            if (boatIsOddOrEven == "odd"){
+                if (arrOfPerson[j].role == "passenger"){
+                    lifeboat[i].push(arrOfPerson[j].name);
+                    passengerCounter++;
+                    personInBoat++;
+                    arrIndexToBeRemoved.push(j);
+                }
+                else if(arrOfPerson[j].role == "crew"){
+                    if (passengerCounter < passengerMin+1 && personInBoat > 4){
+                        continue;
+                    } else {
+                        lifeboat[i].push(arrOfPerson[j].name);
+                        personInBoat++;
+                        arrIndexToBeRemoved.push(j);
+                    }
+                }
+                if(howManyPersonLeft == j+1){
+                    break;
+                }
+            }
+            else if (boatIsOddOrEven == "even"){
+                if (arrOfPerson[j].role == "passenger"){
+                    lifeboat[i].push(arrOfPerson[j].name);
+                    passengerCounter++;
+                    personInBoat++;
+                    arrIndexToBeRemoved.push(j);
+                }
+                else if(arrOfPerson[j].role == "crew"){
+                    if (passengerCounter < passengerMin+1 && personInBoat > 4){
+                        continue;
+                    } else {
+                        lifeboat[i].push(arrOfPerson[j].name);
+                        personInBoat++;
+                        arrIndexToBeRemoved.push(j);
+                    }
+                }
+                if(howManyPersonLeft == j+1){
+                    break;
+                }
+            }
+        }
+        if (boatIsOddOrEven == "odd"){
+            var deleted=0;
+            for (var x = 0; x<arrIndexToBeRemoved.length; x++){
+                allOdd.splice(arrIndexToBeRemoved[x]-deleted, 1);
+                deleted++;
+            }
+        }
+        else if (boatIsOddOrEven == "even"){
+            var deleted=0;
+            for (var x = 0; x<arrIndexToBeRemoved.length; x++){
+                allEven.splice(arrIndexToBeRemoved[x]-deleted, 1);
+                deleted++;
+            }
+        }
+    }
+
+    console.log("Final Output : ");
+    console.log(JSON.stringify(lifeboat));
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 Titanic_Lifeboat
+
+Welcome aboard! To the unsinkable ship, Titanic 2.0!
+
+Please take a seat while the captain and his crews are planning to arrange each one of you to the lifeboat.
+
+Seriously?
+
+Well, it's just, you know, in case of something bad happen, we know what to do.
+
+First, we are going to sort all of you from the youngest to the oldest, both the passengers and the crews.
+
+Second, for the top 8, congratulations! You are chosen to ride our next gen ship, a lifeboat.
+
+Third, well, the number of lifeboat provided in this case is, secret.
+
+Fourth, no matter what happen, the captain must be saved!
+
+And that's it! Please enjoy your journey, bon voyage!
+
+Requirements:
+node.js, make sure it's the latest version. I'm using v8.10.0 in this case.
+
+How to use:
+Simply run using terminal/console "node Case1.js" for Case1, "node Case2.js" for Case2, "node Case3.js" for Case3.
